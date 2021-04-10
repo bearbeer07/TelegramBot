5d30041232eb1a41e2faa4fb7478ea4f5d029224
Igor Dmitriev <Dmitriev.I.Alek@sberbank.ru>
2021-04-10 17:19:37 +0300

    .

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,6 +1,7 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -10,31 +11,35 @@ import java.util.ArrayList;
 public class Bot extends TelegramLongPollingBot {
     ServiceRequest serviceRequest = new ServiceRequest();
     private long chat_id;
-
-    String lastmessage = "";
     ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
 
     @Override
-    public void onUpdateReceived(Update update) {
-        update.getUpdateId();
-
-        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
+   /* public void onUpdateReceived(Update update) {
         chat_id = update.getMessage().getChatId();
+        SendMessage sendMessage = new SendMessage().setChatId(chat_id).setText(getMessage(update.getMessage().getText()));
         sendMessage.setText(input(update.getMessage().getText()));
-
-
         String text = update.getMessage().getText();
         sendMessage.setReplyMarkup(replyKeyboardMarkup);
-/*
-        if (update.getMessage().getText().equalsIgnoreCase("/start")) {
-            sendMessage.setText("Добро пожаловать в бота по приему сервисных заявок");*/
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-        //  }
+    }*/
+
+    public void onUpdateReceived(Update update) {
+        chat_id = update.getMessage().getChatId();
+
+        SendMessage sendMessage = new SendMessage().setChatId(chat_id).setText(getMessage(update.getMessage().getText()));
+      //  sendMessage.setText(input();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
+
 
     public String getMessage(String msg) {
         ArrayList<KeyboardRow> keyboard = new ArrayList<>();
@@ -55,11 +60,10 @@ public class Bot extends TelegramLongPollingBot {
             keyboard.add(keyboardFirstRow);
             keyboard.add(keyboardSecondRow);
             replyKeyboardMarkup.setKeyboard(keyboard);
-            return "Выберете необходимое вам действие";
+            return "Добро пожаловать в бота по приему сервисных заявок.\n" + "Выберете необходимое вам действие.";
         }
-        return "Если возникли проблемы, воспользуйтесь /start";
+        return "Данная команда не поддерживается ботом.\n"+ "Повторите команду";
     }
-
 
     @Override
     public String getBotUsername() {
@@ -74,7 +78,7 @@ public class Bot extends TelegramLongPollingBot {
     public String input(String msg) {
         if (msg.equalsIgnoreCase("Привет") || msg.equals("/start") ||
                 msg.equalsIgnoreCase("Hello") || msg.equalsIgnoreCase("Hi")) {
-            return "Добро пожаловать в бота по приему сервисных заявок";
+            return "Добро пожаловать в бота по приему сервисных заявок.\n" + "Выберете необходимое вам действие.";
         }
         return msg;
     }
